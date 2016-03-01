40fde5b1d966d74cd3de9370f62697700d9b0046
Nathan Oehlman <nathan.oehlman@gmail.com>
2016-03-01 16:08:52 +1000

    Update sleuth.sh to use the current os platform if none provided

--- sleuth.sh
+++ sleuth.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+var os = require('os');
 var sleuth = require('./');
 var args = process.argv.slice(2);
 
 if (args.length === 0) return;
 
 sleuth.discover(args[0], {
-	channel: (args.length > 1 ? args[1] : undefined)
+	channel: (args.length > 1 ? args[1] : undefined),
+	platform: (args.length > 2 ? args[2] : os.platform())
 }, function(err, info) {
 	if (err || !info) return;
 
